09a393d5139fa96ec2b4178121a472afcc814038
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2022-11-17 23:25:15 -0300

    refactor: export data and apply MIT as license

--- packages/grabkit/src/index.ts
+++ packages/grabkit/src/index.ts
@@ -1,1 +1,9 @@
 export { default } from './grabkit';
+
+export { default as AnyRecord } from './AnyRecord';
+export { default as Call } from './Call';
+export { default as Endpoint } from './Endpoint';
+export { default as HttpMethod } from './HttpMethod';
+export { default as StatusCode } from './StatusCode';
+export { default as UrlShape } from './UrlShape';
+export { default as ClosureOptions } from './ClosureOptions';
